31b38e56680e03d9294603e8c653a8fe125c6e01
Benioslawpl <ben.jed2@wp.pl>
2025-09-12 19:40:27 +0200

    aa

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
 
 import "./globals.css";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
   return (
     <div>Strona głowna sdasdasd</div>
-  );
+  )
 }
+
+
+export default function HomePage() {
+  const router = useRouter();
+
+  const goToApi = () => {
+    router.push("/api/machines"); // przekierowanie do route.js
+  };
+
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Start</h1>
+      <button
+        onClick={goToApi}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Pokaż dane z API
+      </button>
+    </div>
+  );}
+  
+
