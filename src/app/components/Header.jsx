a611582521d394224160af91064a2f6bd166cf26
Benioslawpl <ben.jed2@wp.pl>
2025-09-12 21:19:39 +0200

    aa

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,15 +4,20 @@ import Link from "next/link";
 const Header = () => {
     return(
         <header className="header">
-          <nav>
-            <ul className="menu">
-              <li><Link href="/">🏠 Home</Link></li>
-              <li><Link href="/machines">🚜 Maszyny</Link></li>
+          <div className="menu1">
+            <ul>
+              <li><Link href="/"> Home</Link></li>
+            </ul>
+          </div>
+
+          <div className="menu2">
+            <ul>
+              <li><Link href="/machines"> Maszyny</Link></li>
               <li><Link href="/Brygady">Brygady</Link></li>
               <li><Link href="/Sprzet">Sprzęt</Link></li>
-              <li><Link href="/login">🔑 Login</Link></li>
+              <li><Link href="/login"> Login</Link></li>
             </ul>
-          </nav>
+          </div>
         </header>
     )
 }
