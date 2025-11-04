31b66cc82ec17b34082398c8e9ad8db386aab797
Benioslawpl <ben.jed2@wp.pl>
2025-11-04 18:58:57 +0100

    brygady9

--- src/app/pages/brygady/page.js
+++ src/app/pages/brygady/page.js
@@ -111,9 +111,9 @@ export default function BrygadyPage() {
                   <td>{r.brygadzista}</td>
                   <td>{r.created_at ? String(r.created_at).slice(0, 19).replace("T", " ") : "-"}</td>
                   <td className="actionsCell">
-                    <button onClick={() => handleEdit(r)}>✏️ Edytuj</button>
-                    <button className="danger" onClick={() => handleDelete(r.id)}>🗑 Usuń</button>
-                    <Link href={`/pages/brygady/${r.id}`} style={{ marginLeft: 8 }}>Szczegóły →</Link>
+                      <Link href={`/brygady/${r.id}`} className="info-btn">ℹ️</Link>
+                      <button onClick={() => handleEdit(r)}>✏️ Edytuj</button>
+                      <button className="danger" onClick={() => handleDelete(r.id)}>🗑 Usuń</button>   
                   </td>
                 </tr>
               ))}
