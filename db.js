4cbc77a142ac0a30a166ed82cd132fbf148a65be
Beniamin <ben.jed2@wp.pl>
2026-04-21 19:56:16 +0200

    asa

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const { Pool } = pkg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 5,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000,
 });
 
 export default pool;
