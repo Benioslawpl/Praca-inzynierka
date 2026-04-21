12e403f5fe589423e62c1ad0880e041de7979b3f
Beniamin <ben.jed2@wp.pl>
2026-04-21 21:44:33 +0200

    g

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 export default function HomePage() {
   return (
-    <div className="home">
-      <h1>Witamy w aplikacji do zarządzania maszynami </h1>
+    <section className="home">
+      <h1>Witamy w aplikacji do zarządzania maszynami</h1>
       <p>
-        To jest strona główna. Skorzystaj z menu powyżej, aby przejść do
-        sekcji <b>Maszyny</b> lub <b>Login</b>. asdasdasdasdasd
+        To jest strona główna systemu. Skorzystaj z nawigacji u góry, aby
+        przejść do sekcji <b>Maszyny</b>, <b>Brygady</b>, <b>Sprzęt</b> i
+        pozostałych modułów.
       </p>
-    </div>
+    </section>
   );
 }
