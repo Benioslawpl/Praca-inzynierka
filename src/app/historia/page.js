fee5874010372118676aadc61e3d229cb1ee244b
Beniamin <ben.jed2@wp.pl>
2026-04-24 22:11:13 +0200

    h

--- src/app/historia/page.js
+++ src/app/historia/page.js
@@ -6,7 +6,6 @@ export default function HistoriaPage() {
   return (
     <section className="historyPage">
       <div className="historyPageIntro">
-        <span className="historyEyebrow">Dziennik systemu</span>
         <h1>Historia</h1>
         <p>
           Ostatnie działania w aplikacji. Możesz szybko zawęzić widok po obszarze
--- temp-hash.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const password = "Test123!"; // tu wpisz hasło, które chcesz zhashować
-
-const hash = await bcrypt.hash(password, 10);
-console.log("Hasło:", password);
-console.log("Hash bcrypt:", hash);
